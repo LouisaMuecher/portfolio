3e3bee43a5bffa5ab47beab6bb7355c52d5b68f4
LouisaMuecher <116157753+LouisaMuecher@users.noreply.github.com>
2024-11-10 17:41:45 +0100

    projects

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,12 @@ import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
 import { AboutComponent } from './about/about.component';
 import { SkillsComponent } from './skills/skills.component';
+import { ProjectsComponent } from './projects/projects.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, AboutComponent, SkillsComponent],
+  imports: [RouterOutlet, HeaderComponent, AboutComponent, SkillsComponent, ProjectsComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
