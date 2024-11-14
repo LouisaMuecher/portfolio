4d21457b5c84b789be85810dbb6ddc9fde20447a
LouisaMuecher <116157753+LouisaMuecher@users.noreply.github.com>
2024-11-14 21:37:26 +0100

    app and image changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
 import { AboutComponent } from './about/about.component';
 import { SkillsComponent } from './skills/skills.component';
@@ -8,7 +7,7 @@ import { ProjectsComponent } from './projects/projects.component';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, AboutComponent, SkillsComponent, ProjectsComponent],
+  imports: [HeaderComponent, AboutComponent, SkillsComponent, ProjectsComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
