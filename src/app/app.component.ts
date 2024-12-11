78ac03f32a9a925a39765d1afeb9d524c23a37fd
LouisaMuecher <116157753+LouisaMuecher@users.noreply.github.com>
2024-12-11 16:44:33 +0100

    added footer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,11 +4,12 @@ import { AboutComponent } from './about/about.component';
 import { SkillsComponent } from './skills/skills.component';
 import { ProjectsComponent } from './projects/projects.component';
 import { ContactComponent } from './contact/contact.component';
+import { FooterComponent } from './footer/footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [HeaderComponent, AboutComponent, SkillsComponent, ProjectsComponent, ContactComponent],
+  imports: [HeaderComponent, AboutComponent, SkillsComponent, ProjectsComponent, ContactComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
